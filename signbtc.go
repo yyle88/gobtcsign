28b2f4d1d1f0b727631784906ec31f783fdcc489
yangyile <example@gogit.com>
2024-06-07 09:35:58 +0700

    修改签名逻辑

--- signbtc.go
+++ signbtc.go
@@ -12,14 +12,14 @@ import (
 	"github.com/pkg/errors"
 )
 
-// CustomParam 这是客户自定义的参数类型
+// CustomParam 这是客户自定义的参数类型，表示要转入和转出的信息
 type CustomParam struct {
-	VinList []VinType
-	OutList []OutType
+	VinList []VinType //要转入进BTC节点的
+	OutList []OutType //要从BTC节点转出的-这里面通常包含1个目标（转账）和1个自己（找零）
 }
 
 type VinType struct {
-	Utxo     *wire.OutPoint
+	OutPoint *wire.OutPoint //UTXO的主要信息
 	PkScript []byte
 	Amount   int64
 }
@@ -27,19 +27,6 @@ type VinType struct {
 type OutType struct {
 	Address string
 	Amount  int64
-}
-
-// SignParam 这是系统内的参数类型，通过客户自定义的类型能得到待签名的信息
-type SignParam struct {
-	msgTx     *wire.MsgTx
-	pkScripts [][]byte
-	amounts   []int64
-	netParams *chaincfg.Params
-}
-
-// GetMsgTx 通常用户是不需要修改这个值的，直接就能签名，但是假如用户需要修改的话也可以通过这个函数得到它
-func (p *SignParam) GetMsgTx() *wire.MsgTx {
-	return p.msgTx
 }
 
 // NewSignParam 根据用户的输入信息拼接交易
@@ -51,7 +38,7 @@ func NewSignParam(param CustomParam, netParams *chaincfg.Params) (*SignParam, er
 		pkScripts = append(pkScripts, input.PkScript)
 		amounts = append(amounts, input.Amount)
 
-		utxo := input.Utxo
+		utxo := input.OutPoint
 		txIn := wire.NewTxIn(wire.NewOutPoint(&utxo.Hash, uint32(utxo.Index)), nil, nil)
 		//txIn.Sequence = param.UtxoTxInSequence //这里不设置也行，设置是为了重发交易
 		msgTx.AddTxIn(txIn)
@@ -68,11 +55,19 @@ func NewSignParam(param CustomParam, netParams *chaincfg.Params) (*SignParam, er
 		msgTx.AddTxOut(wire.NewTxOut(output.Amount, pkScript))
 	}
 	return &SignParam{
-		msgTx:     msgTx,
-		pkScripts: pkScripts,
-		amounts:   amounts,
-		netParams: netParams,
+		MsgTx:     msgTx,
+		PkScripts: pkScripts,
+		Amounts:   amounts,
+		NetParams: netParams,
 	}, nil
+}
+
+// SignParam 这是待签名的交易信息，基本上最核心的信息就是这些，通过前面的逻辑能构造出这个结构，通过这个结构即可签名，签名后即可发交易
+type SignParam struct {
+	MsgTx     *wire.MsgTx
+	PkScripts [][]byte
+	Amounts   []int64
+	NetParams *chaincfg.Params
 }
 
 // Sign 根据钱包地址和钱包私钥签名
@@ -83,7 +78,7 @@ func Sign(fromAddress string, privateKeyHex string, param *SignParam) error {
 	}
 	privKey, pubKey := btcec.PrivKeyFromBytes(privKeyBytes)
 
-	walletAddress, err := btcutil.DecodeAddress(fromAddress, param.netParams)
+	walletAddress, err := btcutil.DecodeAddress(fromAddress, param.NetParams)
 	if err != nil {
 		return errors.WithMessage(err, "wrong from_address")
 	}
@@ -91,7 +86,7 @@ func Sign(fromAddress string, privateKeyHex string, param *SignParam) error {
 	switch address := walletAddress; address.(type) {
 	case *btcutil.AddressPubKeyHash: //请参考 txscript.PubKeyHashTy 的签名逻辑
 		//检查钱包的地址是不是压缩的，有压缩和不压缩两种格式的地址，都是可以用的
-		compress, err := CheckPKHAddressIsCompress(param.netParams, pubKey, fromAddress)
+		compress, err := CheckPKHAddressIsCompress(param.NetParams, pubKey, fromAddress)
 		if err != nil {
 			return errors.WithMessage(err, "wrong sign check_from_address_is_compress")
 		}
@@ -127,9 +122,9 @@ func CheckPKHAddressIsCompress(defaultNet *chaincfg.Params, publicKey *btcec.Pub
 
 func SignP2PKH(signParam *SignParam, privKey *btcec.PrivateKey, compress bool) error {
 	var (
-		msgTx     = signParam.msgTx
-		pkScripts = signParam.pkScripts
-		amounts   = signParam.amounts
+		msgTx     = signParam.MsgTx
+		pkScripts = signParam.PkScripts
+		amounts   = signParam.Amounts
 	)
 
 	for idx := range msgTx.TxIn {
