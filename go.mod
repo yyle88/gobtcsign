093f6db55a58e6b7b3f7d04d3f5fdafc7d9e14e3
yangyile <example@gogit.com>
2024-06-26 19:01:16 +0700

    添加比特币交易预估大小的逻辑

--- go.mod
+++ go.mod
@@ -6,17 +6,22 @@ require (
 	github.com/btcsuite/btcd v0.24.2
 	github.com/btcsuite/btcd/btcec/v2 v2.3.3
 	github.com/btcsuite/btcd/btcutil v1.1.5
+	github.com/btcsuite/btcwallet/wallet/txrules v1.2.1
+	github.com/btcsuite/btcwallet/wallet/txsizes v1.2.4
 	github.com/pkg/errors v0.9.1
-	github.com/stretchr/testify v1.8.4
+	github.com/stretchr/testify v1.9.0
 )
 
 require (
+	github.com/aead/siphash v1.0.1 // indirect
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
 	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
+	github.com/kkdai/bstream v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ txfee.go
@@ -0,0 +1,79 @@
+package gosignbtc
+
+import (
+	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcwallet/wallet/txrules"
+	"github.com/btcsuite/btcwallet/wallet/txsizes"
+	"github.com/pkg/errors"
+)
+
+// CalculateMsgTxFee 通过未签名且未找零的交易，预估出需要的费用
+// 代码基本是仿照这里的 github.com/btcsuite/btcwallet/wallet/txauthor@v1.3.4/author.go 里面 NewUnsignedTransaction 的逻辑
+// 需要特别注意的是，这里只是个使用的样例
+// 由于是计算手续费的，因为这个交易里不应该包含找零的 output 信息，否则结果是无意义的
+func CalculateMsgTxFee(msgTx *wire.MsgTx, changeAddress btcutil.Address, feeRatePerKb btcutil.Amount) (btcutil.Amount, error) {
+	maxSignedSize, err := CalculateMsgTxSize(msgTx, changeAddress)
+	if err != nil {
+		return 0, errors.WithMessage(err, "calculate size is wrong")
+	}
+	maxRequiredFee := txrules.FeeForSerializeSize(feeRatePerKb, maxSignedSize)
+	return maxRequiredFee, nil
+}
+
+// CalculateMsgTxSize 计算交易的预估大小（在最坏情况下的预估大小）
+// 这个函数还是抄的 github.com/btcsuite/btcwallet/wallet/txauthor@v1.3.4/author.go 里面 NewUnsignedTransaction 的逻辑
+// 这个函数也是个样例，主要是演示如何预估大小，当然也是可以直接使用的
+// 这个交易里不应该包含找零信息
+func CalculateMsgTxSize(msgTx *wire.MsgTx, changeAddress btcutil.Address) (int, error) {
+	var scripts = make([][]byte, 0, len(msgTx.TxIn))
+	for _, txIn := range msgTx.TxIn {
+		scripts = append(scripts, txIn.SignatureScript)
+	}
+	return CalculateSize(scripts, msgTx.TxOut, changeAddress)
+}
+
+// CalculateSize 计算交易的预估大小（在最坏情况下的预估大小）
+// 这个函数的参数详见前面函数的调用
+func CalculateSize(scripts [][]byte, outputs []*wire.TxOut, changeAddress btcutil.Address) (int, error) {
+	changeAddressScript, err := txscript.PayToAddrScript(changeAddress)
+	if err != nil {
+		return 0, errors.WithMessage(err, "wrong change_address")
+	}
+	var scriptSize int
+	switch {
+	case txscript.IsPayToPubKeyHash(changeAddressScript):
+		scriptSize = txsizes.P2PKHPkScriptSize
+	case txscript.IsPayToScriptHash(changeAddressScript):
+		scriptSize = txsizes.NestedP2WPKHPkScriptSize
+	case txscript.IsPayToWitnessPubKeyHash(changeAddressScript), txscript.IsPayToWitnessScriptHash(changeAddressScript):
+		scriptSize = txsizes.P2WPKHPkScriptSize
+	case txscript.IsPayToTaproot(changeAddressScript):
+		scriptSize = txsizes.P2TRPkScriptSize
+	default:
+		return 0, errors.New("UNSUPPORTED ADDRESS TYPE")
+	}
+
+	// We count the types of inputs, which we'll use to estimate
+	// the vsize of the transaction.
+	var nested, p2wpkh, p2tr, p2pkh int
+	for _, pkScript := range scripts {
+		switch {
+		// If this is a p2sh output, we assume this is a
+		// nested P2WKH.
+		case txscript.IsPayToScriptHash(pkScript):
+			nested++
+		case txscript.IsPayToWitnessPubKeyHash(pkScript):
+			p2wpkh++
+		case txscript.IsPayToTaproot(pkScript):
+			p2tr++
+		default:
+			p2pkh++
+		}
+	}
+	maxSignedSize := txsizes.EstimateVirtualSize(
+		p2pkh, p2tr, p2wpkh, nested, outputs, scriptSize,
+	)
+	return maxSignedSize, nil
+}
