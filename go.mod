84853814df81c0dc750548faecd4f757b67b70a1
yangyile <example@gogit.com>
2024-09-17 16:48:21 +0700

    增加灰尘判定逻辑

--- go.mod
+++ go.mod
@@ -17,12 +17,12 @@ require (
 	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
 	github.com/btcsuite/btclog v0.0.0-20170628155309-84c8d2346e9f // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/decred/dcrd/crypto/blake256 v1.0.1 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.1.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
 	github.com/kkdai/bstream v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- utils.go
+++ utils.go
@@ -4,7 +4,11 @@ import (
 	"bytes"
 	"encoding/hex"
 
+	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
+	"github.com/btcsuite/btcwallet/wallet/txrules"
 	"github.com/pkg/errors"
 )
 
@@ -39,4 +43,38 @@ func NewMsgTxFromTxHex(txHex string) (*wire.MsgTx, error) {
 		return nil, errors.WithMessage(err, "wrong deserialize")
 	}
 	return msgTx, nil
+}
+
+// IsDustOutputBtc 检查是不是灰尘输出，链不允许灰尘输出，避免到处都是粉尘
+func IsDustOutputBtc(netParam *chaincfg.Params, address string, amount int64, dustLimitCoin float64, feePerKb btcutil.Amount) (bool, error) {
+	dustLimitAmount, err := btcutil.NewAmount(dustLimitCoin)
+	if err != nil {
+		return false, errors.WithMessage(err, "wrong dust limit coin to amount")
+	}
+	if btcutil.Amount(amount) < dustLimitAmount { //当小于硬性灰尘数量时，就肯定是灰尘的
+		return true, nil
+	}
+	addressX, err := btcutil.DecodeAddress(address, netParam)
+	if err != nil {
+		return false, errors.WithMessage(err, "wrong address")
+	}
+	pkScript, err := txscript.PayToAddrScript(addressX)
+	if err != nil {
+		return false, errors.WithMessage(err, "wrong script")
+	}
+	output := wire.NewTxOut(amount, pkScript)
+	isDust := txrules.IsDustOutput(output, feePerKb)
+	return isDust, nil
+}
+
+// IsDustOutputDoge 根据 https://github.com/dogecoin/dogecoin/blob/master/doc/fee-recommendation.md 这个看到只有两个规则
+func IsDustOutputDoge(amount int64, dustLimitCoin float64) (bool, error) {
+	dustLimitAmount, err := btcutil.NewAmount(dustLimitCoin)
+	if err != nil {
+		return false, errors.WithMessage(err, "wrong dust limit coin to amount")
+	}
+	if btcutil.Amount(amount) < dustLimitAmount { //当小于硬性灰尘数量时，就肯定是灰尘的
+		return true, nil
+	}
+	return false, nil
 }
